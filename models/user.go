a6a141b276bc2fe8011e275d4077ffc800819c44
ricomateo <mrico@fi.uba.ar>
2025-04-06 18:11:22 -0300

    remove outdated TO-DO

--- models/user.go
+++ models/user.go
@@ -3,6 +3,6 @@ package models
 type User struct {
 	Name     string `json:"name" binding:"required"`
 	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"` // TODO: change this to receive the hash of the password
+	Password string `json:"password" binding:"required"`
 	UserType string `json:"userType" binding:"required"`
 }
