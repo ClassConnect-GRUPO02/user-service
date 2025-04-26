176789a22bca524dd283ef87999afe4302d685ce
ricomateo <mrico@fi.uba.ar>
2025-04-26 20:31:06 -0300

    add router tests with repository errors

--- router/router_test.go
+++ router/router_test.go
@@ -189,26 +189,6 @@ func TestUserLogin(t *testing.T) {
 		assert.Equal(t, http.StatusForbidden, w.Code)
 	})
 
-	t.Run("Login fails due to internal server error", func(t *testing.T) {
-		userRepositoryMock := new(mocks.Repository)
-		mockError := fmt.Errorf("mock error")
-		userRepositoryMock.On("IsEmailRegistered", mock.Anything).Return(false, mockError)
-
-		userService, err := service.NewService(userRepositoryMock, &config)
-		assert.NoError(t, err)
-		handler := handlers.NewUserHandler(userService)
-
-		router := gin.Default()
-		router.POST("/login", handler.HandleLogin)
-
-		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("POST", "/login", strings.NewReader(body))
-		req.Header.Set("Content-Type", "application/json")
-
-		router.ServeHTTP(w, req)
-		assert.Equal(t, http.StatusInternalServerError, w.Code)
-	})
-
 	t.Run("Request with missing parameters returns bad request", func(t *testing.T) {
 		userRepositoryMock := new(mocks.Repository)
 
@@ -392,5 +372,159 @@ func TestGetUser(t *testing.T) {
 
 		router.ServeHTTP(w, req)
 		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+}
+
+func TestUserCreationWithInternalServerError(t *testing.T) {
+	config := config.Config{}
+	mockError := fmt.Errorf("mock error")
+	body := `{"name":"John Doe","email":"john@example.com", "userType": "alumno", "password":"1234", "latitude": 10000, "longitude": -10000}`
+
+	// Test valid login
+	t.Run("Create user returns internal server error when IsEmailRegistered returns error", func(t *testing.T) {
+		userRepositoryMock := new(mocks.Repository)
+		userRepositoryMock.On("IsEmailRegistered", mock.Anything).Return(false, mockError)
+		userService, err := service.NewService(userRepositoryMock, &config)
+		assert.NoError(t, err)
+		handler := handlers.NewUserHandler(userService)
+
+		router := gin.Default()
+		router.POST("/users", handler.CreateUser)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/users", strings.NewReader(body))
+		req.Header.Set("Content-Type", "application/json")
+
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+
+	t.Run("Create user returns internal server error when AddUser returns error", func(t *testing.T) {
+		userRepositoryMock := new(mocks.Repository)
+		userRepositoryMock.On("IsEmailRegistered", mock.Anything).Return(false, nil)
+		userRepositoryMock.On("AddUser", mock.Anything).Return(mockError)
+		userService, err := service.NewService(userRepositoryMock, &config)
+		assert.NoError(t, err)
+		handler := handlers.NewUserHandler(userService)
+
+		router := gin.Default()
+		router.POST("/users", handler.CreateUser)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/users", strings.NewReader(body))
+		req.Header.Set("Content-Type", "application/json")
+
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+}
+
+func TestUserLoginWithInternalServerError(t *testing.T) {
+	config := config.Config{BlockingDuration: 120}
+	body := `{"email":"john@example.com", "password":"1234"}`
+
+	t.Run("User logged in successfully", func(t *testing.T) {
+		userRepositoryMock := new(mocks.Repository)
+		userRepositoryMock.On("IsEmailRegistered", mock.Anything).Return(true, nil)
+		userRepositoryMock.On("UserBlockedUntil", mock.Anything).Return(int64(0), nil)
+		userRepositoryMock.On("PasswordMatches", mock.Anything, mock.Anything).Return(true, nil)
+		userRepositoryMock.On("GetUserIdByEmail", mock.Anything).Return("1", nil)
+
+		userService, err := service.NewService(userRepositoryMock, &config)
+		assert.NoError(t, err)
+		handler := handlers.NewUserHandler(userService)
+
+		router := gin.Default()
+		router.POST("/login", handler.HandleLogin)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/login", strings.NewReader(body))
+		req.Header.Set("Content-Type", "application/json")
+
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusAccepted, w.Code)
+	})
+
+	mockError := fmt.Errorf("mock error")
+	t.Run("Login fails with internal server error when IsEmailRegistered returns an error", func(t *testing.T) {
+		userRepositoryMock := new(mocks.Repository)
+		userRepositoryMock.On("IsEmailRegistered", mock.Anything).Return(false, mockError)
+
+		userService, err := service.NewService(userRepositoryMock, &config)
+		assert.NoError(t, err)
+		handler := handlers.NewUserHandler(userService)
+
+		router := gin.Default()
+		router.POST("/login", handler.HandleLogin)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/login", strings.NewReader(body))
+		req.Header.Set("Content-Type", "application/json")
+
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+
+	t.Run("Login fails with internal server error when UserBlockedUntil returns an error", func(t *testing.T) {
+		userRepositoryMock := new(mocks.Repository)
+		userRepositoryMock.On("IsEmailRegistered", mock.Anything).Return(true, nil)
+		userRepositoryMock.On("UserBlockedUntil", mock.Anything).Return(int64(0), mockError)
+
+		userService, err := service.NewService(userRepositoryMock, &config)
+		assert.NoError(t, err)
+		handler := handlers.NewUserHandler(userService)
+
+		router := gin.Default()
+		router.POST("/login", handler.HandleLogin)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/login", strings.NewReader(body))
+		req.Header.Set("Content-Type", "application/json")
+
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+
+	t.Run("Login fails with internal server error when PasswordMatches returns an error", func(t *testing.T) {
+		userRepositoryMock := new(mocks.Repository)
+		userRepositoryMock.On("IsEmailRegistered", mock.Anything).Return(true, nil)
+		userRepositoryMock.On("UserBlockedUntil", mock.Anything).Return(int64(0), nil)
+		userRepositoryMock.On("PasswordMatches", mock.Anything, mock.Anything).Return(false, mockError)
+
+		userService, err := service.NewService(userRepositoryMock, &config)
+		assert.NoError(t, err)
+		handler := handlers.NewUserHandler(userService)
+
+		router := gin.Default()
+		router.POST("/login", handler.HandleLogin)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/login", strings.NewReader(body))
+		req.Header.Set("Content-Type", "application/json")
+
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+
+	t.Run("User logged in successfully", func(t *testing.T) {
+		userRepositoryMock := new(mocks.Repository)
+		userRepositoryMock.On("IsEmailRegistered", mock.Anything).Return(true, nil)
+		userRepositoryMock.On("UserBlockedUntil", mock.Anything).Return(int64(0), nil)
+		userRepositoryMock.On("PasswordMatches", mock.Anything, mock.Anything).Return(true, nil)
+		userRepositoryMock.On("GetUserIdByEmail", mock.Anything).Return("", mockError)
+
+		userService, err := service.NewService(userRepositoryMock, &config)
+		assert.NoError(t, err)
+		handler := handlers.NewUserHandler(userService)
+
+		router := gin.Default()
+		router.POST("/login", handler.HandleLogin)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("POST", "/login", strings.NewReader(body))
+		req.Header.Set("Content-Type", "application/json")
+
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
 	})
 }
