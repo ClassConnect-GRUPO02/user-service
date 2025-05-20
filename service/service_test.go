cc1355a02cb4f3b41da409a4bffcd796f8807c13
ricomateo <mrico@fi.uba.ar>
2025-05-19 22:44:21 -0300

    test: service.UserIsBlocked

--- service/service_test.go
+++ service/service_test.go
@@ -673,3 +673,33 @@ func TestServiceIssueToken(t *testing.T) {
 		assert.Equal(t, userType, tokenClaims.UserType)
 	})
 }
+
+func TestServiceUserIsBlocked(t *testing.T) {
+	config := config.Config{BlockingDuration: 300}
+
+	t.Run("Service.UserIsBlocked returns false when blockedUntil is 0", func(t *testing.T) {
+		blockedUntil := int64(0)
+		userRepositoryMock := new(mocks.Repository)
+		userRepositoryMock.On("UserBlockedUntil", mock.Anything).Return(blockedUntil, nil)
+
+		userService, err := service.NewService(userRepositoryMock, &config)
+		assert.NoError(t, err)
+
+		userIsBlocked, err := userService.UserIsBlocked(utils.TEST_EMAIL)
+		assert.NoError(t, err)
+		assert.False(t, userIsBlocked)
+	})
+
+	t.Run("Service.UserIsBlocked returns true when blockedUntil is greater than timestamp no", func(t *testing.T) {
+		blockedUntil := int64(time.Now().Unix() + 300)
+		userRepositoryMock := new(mocks.Repository)
+		userRepositoryMock.On("UserBlockedUntil", mock.Anything).Return(blockedUntil, nil)
+
+		userService, err := service.NewService(userRepositoryMock, &config)
+		assert.NoError(t, err)
+
+		userIsBlocked, err := userService.UserIsBlocked(utils.TEST_EMAIL)
+		assert.NoError(t, err)
+		assert.True(t, userIsBlocked)
+	})
+}
